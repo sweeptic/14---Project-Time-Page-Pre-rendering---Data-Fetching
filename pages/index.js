c7a25ca3f772e95432e480e36839d4c39849212e
Attila Horvath <sweeptic@gmail.com>
2025-03-14 13:11:11 +0100

    003 Configuring the head Content

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../helpers/api-util';
 
@@ -6,6 +7,10 @@ function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="this is content meta information" />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
